5c3a1e00b6394fe8537604f53e129fb84019b776
TheusZer0 <quetzal.noc@gmail.com>
2021-07-11 05:04:46 -0400

    CRUD listo para base de datos y scheme de profesores

--- src/connecions/index.connections.js
+++ src/connecions/index.connections.js
@@ -49,11 +49,55 @@ const updateUser = async (req, res) => {
     res.json('User Updated Successfully');
 };
 
+//Profesores
+const getProfesores = async (req, res) => {
+    //recordar cambiar luego de que me entregen la base de datos correcta
+    const response = await pool.query('SELECT * FROM users.profesores'); //mensaje de respuesta
+    console.log(response.rows); //se muestra en formato row
+    res.status(200).json(response.rows); //codigo de status
+};
+
+const createNewProfesores = async (req, res) => {
+    //recordar cambiar luego de que me entregen la base de datos correcta
+    const { user, correo} = (req.body); //se muestra en formato row
+    const response = await pool.query('INSERT INTO users.profesores (name,email) VALUES ($1,$2)',[user,correo]);
+    res.status(200);
+    console.log(response)
+    res.send('Usuario creado'); //codigo de status
+};
+
+const getProfesoresById = async (req, res) => {
+    const response = await pool.query('SELECT * FROM users.profesores WHERE id = $1', [parseInt(req.params.id)]);
+    res.status(200).json(response.rows);
+};
+
+const removeProfesores = async (req, res) => {
+    const id = parseInt(req.params.id)
+    const response = await pool.query('DELETE FROM users.profesores WHERE id = $1', [parseInt(req.params.id)]);
+    console.log(response)
+    res.json(`User ${id} deleted Successfully`);
+};
+
+const updateProfesores = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, email } = req.body;
+    const response =await pool.query('UPDATE users.profesores SET name = $1, email = $2 WHERE id = $3', [
+        name,
+        email,
+        id
+    ]);
+    res.json('User Updated Successfully');
+};
 
 module.exports = {
     getUsers,
     createNewUsers,
     getUsersById,
     removeUsers,
-    updateUser
+    updateUser,
+    getProfesores,
+    createNewProfesores,
+    getProfesoresById,
+    removeProfesores,
+    updateProfesores,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,13 @@ const {
     createNewUsers,
     getUsersById,
     removeUsers,
-    updateUser
-} = require('../connecions/index.connecions');
+    updateUser,
+    getProfesores,
+    getProfesoresById,
+    createNewProfesores,
+    removeProfesores,
+    updateProfesores
+} = require('../connecions/index.connections');
 
 router.get('/', ((req, res) =>
 {
@@ -15,18 +20,36 @@ router.get('/', ((req, res) =>
 
 /*
 * Rutas para el API, tenemos diferentes rutas
+* */
+
+/*
 * para cada una de las funciones construidas para el proyecto:
 *
 * > getUsers = GET request, muestra todos los usuarios
 * > createNewUsers = POST request, realiza un INSERT en la BD
 * > removeUsers = REMOVE request, elimina a un usuario de la BD
 * > getUsersById = GET request, encuentra un usuario por ID
-*
+* > updateUser =
 * */
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
 router.post('/users', createNewUsers);
 router.delete('/users/:id', removeUsers);
 router.put('/users/:id', updateUser);
+
+/*
+* para cada una de las funciones construidas para el proyecto:
+*
+* > getProfesores = GET request, muestra todos los Profesores
+* > createNewProfesores = POST request, realiza un INSERT en la BD
+* > removeProfesores = REMOVE request, elimina a un Profesores de la BD
+* > getProfesoresById = GET request, encuentra un Profesores por ID
+*
+* */
+router.get('/profesores', getProfesores);
+router.get('/profesores/:id', getProfesoresById);
+router.post('/profesores', createNewProfesores);
+router.delete('/profesores/:id', removeProfesores);
+router.put('/profesores/:id', updateProfesores);
 
 module.exports = router;
